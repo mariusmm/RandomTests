6f082e30ff24306d6be517974dc0e7b080c38228
fdomf <fdomenef@gmail.com>
2021-03-22 19:18:57 +0100

    Updated tracker from old branch

--- app/static/tracker.js
+++ app/static/tracker.js
@@ -83,9 +83,10 @@ function update() {
     markers[0].setGeometry(newPoint);
     vectorLayer.getSource().changed();
     
+    if (longitude < 0)
+       longitude = longitude + 360;
+    
     if (longitude != 0 && latitude != 0)
-        if (longitude < 0)
-            longitude = longitude + 360;
         orbitpoints.push([longitude, latitude]);
     
     
@@ -156,7 +157,7 @@ function updateCoords() {
             longitude = data['satellites'][0]['long'];
             elevation = data['satellites'][0]['elevation'];
         },
-         /*   error: function() {
+          /*  error: function() {
             alert("There was a problem with the server. Try again soon!");
         }*/
     });
